e484d839aaa7677dddcd573ced99b6c1def0daac
hsvcollections <hsvcollections18@gmail.com>
2025-03-05 13:06:19 +0530

    Update quiz-search006.js

--- quiz-search005.js
+++ quiz-search005.js
@@ -81,21 +81,4 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-        function filterQuestions() {
-            var selectedType = document.getElementById("question-type").value;
-            var allQuestions = document.querySelectorAll(".quiz-item");
-
-            allQuestions.forEach(function(question) {
-                if (selectedType === "all") {
-                    question.style.display = "block";
-                } else if (question.classList.contains(selectedType)) {
-                    question.style.display = "block";
-                } else {
-                    question.style.display = "none";
-                }
-            });
-        }
-
-        // Show all questions initially
-        filterQuestions();
-
+       
